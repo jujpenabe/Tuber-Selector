435d83e06fe5bd15176da3788f46d3c1e35013e9
Juan José Peña <jujpenabe@unal.edu.co>
2022-05-20 22:39:01 -0500

    Added roundness factor by minCircle area and a prototype video demo

--- Measurement/Estimate_Area.py
+++ Measurement/Estimate_Area.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -57,8 +59,11 @@ def getROI(image, mask):
             # Convert the polygon to a bounding rectangle:
             boundRect = cv2.boundingRect(contoursPoly)
             minRect = cv2.minAreaRect(contoursPoly)
+            (x,y), radius = cv2.minEnclosingCircle(contoursPoly)
             box = cv2.boxPoints(minRect)
             box = np.int0(box)
+            center = (int(x), int(y))
+            radius = int(radius)
             # Set the rectangle dimensions:
             rectangleX = boundRect[0]
             rectangleY = boundRect[1]
@@ -66,21 +71,22 @@ def getROI(image, mask):
             rectangleHeight = boundRect[1] + boundRect[3]
             
             # Draw the rectangle:
-            cv2.rectangle(polyRectImage, (int(rectangleX), int(rectangleY)), (int(rectangleWidth), int(rectangleHeight)), (0, 255, 0), 2)
-            cv2.drawContours(polyRectImage, [box], 0, (255,0,0),2)
-            #show(polyRectImage, "Poly Rectangle")
+            cv2.rectangle(polyRectImage, (int(rectangleX), int(rectangleY)), (int(rectangleWidth), int(rectangleHeight)), (0, 255, 0), 5)
+            cv2.drawContours(polyRectImage, [box], 0, (255,0,0),5)
+            cv2.circle(polyRectImage, center, radius, (0,0,255), 5)
             
             # Get Roundness
             # print("minRect data: " + str(minRect))
             # print("boundRect data: " +str(boundRect))
-            minArea = minRect[1][0] * minRect[1][1]
-            badArea = boundRect[2] * boundRect[3]
-
+            minRectArea = minRect[1][0] * minRect[1][1]
+            straightArea = boundRect[2] * boundRect[3]
+            minCircleArea = radius * radius * math.pi
+            
             #Crop the ROI:
             croppedImg = image[rectangleY:rectangleHeight, rectangleX:rectangleWidth]
             #croppedImg = segmented[rectangleY:rectangleWidth, rectangleX:rectangleHeight]      
             #show(croppedImg, "Cropped Image")
-    return croppedImg, contourArea, minArea, badArea
+    return croppedImg, polyRectImage, contourArea, minRectArea, straightArea, minCircleArea
 
 def test():
     file = "../Samples/Renders/blender_potato2_1_light.jpg"
@@ -89,24 +95,26 @@ def test():
     inputImage = cv2.imread(file) [:,:,::-1]
     image1 = prp.hsv_otsu_threshold(inputImage)
     image2 = prp.and_mask(inputImage, image1)
-    image3, contourArea, minArea, badArea = getROI(image2, image1)
+    image3, polyRectImage, contourArea, minArea, badArea, minCircleArea = getROI(image2, image1)
     # plt.imshow(images[0], cmap='gray', vmin=0, vmax=255)
     # plt.show()
     # plt.imshow(images[1], cmap='gray', vmin=0, vmax=255)
     # plt.show()
-    roundness = contourArea / minArea
-    titles = ['Original Image', 'Hue Thresholding', 'Masked','ROI cuadrado']
-    images = [inputImage, image1, image2, image3]
+    squareRoundness = contourArea / minArea
+    circleRoundness = contourArea / minCircleArea
+    titles = ['Original Image', 'Hue Thresholding', 'Masked', 'Enclosing areas', 'ROI cuadrado']
+    images = [inputImage, image1, image2, polyRectImage, image3]
     
     for i in range(len(images)):
-        plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray', vmin=0, vmax=255)
+        plt.subplot(3, 2, i + 1), plt.imshow(images[i], 'gray', vmin=0, vmax=255)
         plt.title(titles[i])
         plt.xticks([]), plt.yticks([])
     
-    plt.text(0, -0.075, "Contour Area= {}".format(contourArea), ha="center", transform=plt.gca().transAxes)
-    plt.text(0, -0.15, "Minimum rectangle area= {}".format(minArea), ha="center", transform=plt.gca().transAxes)
-    plt.text(0, -0.225, "Straight rectangle area= {}".format(badArea), ha="center", transform=plt.gca().transAxes)
-    plt.text(0, -0.3, "Roundness Factor= {}".format(round(roundness, 3)), ha="center", transform=plt.gca().transAxes)
+    plt.text(1.5, 1, "Contour Area= {}".format(contourArea), transform=plt.gca().transAxes)
+    plt.text(1.5, 0.8, "Minimum rectangle area= {}".format(int(minArea)), transform=plt.gca().transAxes)
+    plt.text(1.5, 0.6, "Straight rectangle area= {}".format(badArea), transform=plt.gca().transAxes)
+    plt.text(1.5, 0.4, "Square Roundness Factor= {}".format(round(squareRoundness, 3)), transform=plt.gca().transAxes)
+    plt.text(1.5, 0.2, "Circle Roundness Factor= {}".format(round(circleRoundness, 3)), transform=plt.gca().transAxes)
     plt.show()
 
 if __name__ == '__main__':
